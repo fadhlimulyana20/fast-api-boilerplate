f0ab8970a1e0a1c2c42c6c40b99c5dc010e24a32
Fadhli Mulyana <baba.fadhli@gmail.com>
2022-06-12 09:51:29 +0700

    Aaded versioning router

--- api/api.py
+++ api/api.py
@@ -3,7 +3,14 @@ from api.endpoints import user, item, auth
 
 
 api_router = APIRouter()
+v1_api_router = APIRouter(prefix='/v1')
 
-api_router.include_router(user.router)
-api_router.include_router(auth.router)
+# V1 Router
+v1_api_router.include_router(user.router)
+v1_api_router.include_router(auth.router)
+
+# Main Router 
+api_router.include_router(v1_api_router)
+
+
 # api_router.include_router(item.router)
