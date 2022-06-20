3c6db752566b33c989ab803eca44fe3faeffa00d
Fadhli Mulyana <baba.fadhli@gmail.com>
2022-06-20 09:54:04 +0700

    Added string utils

---
+++ utils/string.py
@@ -0,0 +1,10 @@
+from re import sub
+
+class StringUtils():
+    @staticmethod
+    def snake_case(s: str) -> str:
+        return '_'.join(
+            sub('([A-Z][a-z]+)', r' \1',
+            sub('([A-Z]+)', r' \1',
+            s.replace('-', ' '))).split()).lower()
+
