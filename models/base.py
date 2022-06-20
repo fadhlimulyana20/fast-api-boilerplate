94169f696373e4b4e1d5eeac5025ee786e38bd29
Fadhli Mulyana <baba.fadhli@gmail.com>
2022-06-20 09:54:56 +0700

    Add string utils to model base

--- models/base.py
+++ models/base.py
@@ -2,14 +2,15 @@ from typing import Any
 
 from sqlalchemy.ext.declarative import as_declarative, declared_attr, declarative_base
 
+from utils.string import StringUtils
 
-# @as_declarative()
-# class Base:
-#     id: Any
-#     __name__: str
-#     # Generate __tablename__ automatically
-#     @declared_attr
-#     def __tablename__(cls) -> str:
-#         return cls.__name__.lower()
+@as_declarative()
+class Base:
+    id: Any
+    __name__: str
+    # Generate __tablename__ automatically
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return f"{StringUtils.snake_case(cls.__name__)}s"
 
-Base = declarative_base()
+# Base = declarative_base()
